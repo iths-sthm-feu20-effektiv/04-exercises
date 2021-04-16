9e63e184433823154914ca66695cbae3775e78a2
David Andersson <lejonmanen@gmail.com>
2021-04-16 10:46:24 +0200

    Exercise 4b

--- server.js
+++ server.js
@@ -9,26 +9,29 @@ const PORT = 1340
 // installera middleware
 app.use( cors() )
 app.use( express.json() )
-// static
+
+// Egen middleware: logger
 app.use((req, res, next) => {
 	console.log(`${req.method}  ${req.url} `, req.params)
 	next()
 })
+
+app.use( express.static(path.join(__dirname, 'public')) )
 
 
 // routes
 app.get('/', (req, res) => {
 	res.send('Welcome to exercise 4 server.')
 })
-app.get('/index.html', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public/index.html'))
-})
-app.get('/style.css', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public/style.css'))
-})
-app.get('/script.js', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public/script.js'))
-})
+// app.get('/index.html', (req, res) => {
+// 	res.sendFile(path.join(__dirname, 'public/index.html'))
+// })
+// app.get('/style.css', (req, res) => {
+// 	res.sendFile(path.join(__dirname, 'public/style.css'))
+// })
+// app.get('/script.js', (req, res) => {
+// 	res.sendFile(path.join(__dirname, 'public/script.js'))
+// })
 
 
 // starta webbservern
